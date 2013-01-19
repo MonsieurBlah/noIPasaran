0a232d7ace33172f739c0a90da2a8d457ea944a6
Bernard Debecker <bernard.debecker@gmail.com>
2013-01-19 17:58:43 +0100

    Add About page, licence, update CSS, menu

--- site/requestHandlers.js
+++ site/requestHandlers.js
@@ -1,13 +1,13 @@
 var querystring = require('querystring')
 , dns 			= require('dns')
 , fs 			= require('fs')
-, util 			= require('util')
-, mu 			= require('mustache');
+, util          = require('util')
+, mu 			= require('mu2');
 /*var dns = require('native-dns');*/
 var debug = false;
 
-// The first page
-function start(response, postData) {
+// The home page
+function index(response, postData) {
 	console.log("Request handler 'start' was called.");
     fs.readFile('./index.htm', function(error, content) {
         if (error) {
@@ -27,6 +27,7 @@ function check(response, postData) {
 	console.log("Request handler 'check' was called.");
 
 	var url = querystring.parse(postData).text;
+    mu.root = __dirname + '/site';
 	
 	dns.resolve4(url, function (err, addresses) {
 		if (err) throw err;
@@ -43,10 +44,27 @@ function check(response, postData) {
 		/*var JSONaddresses = JSON.stringify({ip : ips});*/
 		
 		var template = "{{#JSONaddresses}}<b>{{ip}}</b>{{/JSONaddresses}}";
+/*
+        var stream = mu.compileAndRender('check.htm', jsonips);
+        sys.puts('Stream = ' + stream);
+        util.pump(stream, response);
+        console.log('Stream = ' + stream.toString('utf-8'));
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.end(stream, 'utf-8');*/
 
-		var html = mu.to_html(template, jsonips);
+        fs.readFile('./check.htm', function(error, content) {
+            if (error) {
+                response.writeHead(500);
+                response.end();
+            }
+            else {
+                response.end(content, 'utf-8');
+            }
+        })
+
+		/*var html = mu.to_html(template, jsonips);
 		console.log('html = ' + html);
-		/*response.writeHead(200, { 'Content-Type': 'text/html' });
+		response.writeHead(200, { 'Content-Type': 'text/html' });
         response.end(html, 'utf-8');
 		
 		response.end();	*/
@@ -67,6 +85,21 @@ function checkhtm(response, postData) {
     });
 }
 
+function about(response, postData) {
+    console.log("Request handler 'start' was called.");
+    fs.readFile('./about.htm', function(error, content) {
+        if (error) {
+            response.writeHead(500);
+            response.end();
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': 'text/html' });
+            response.end(content, 'utf-8');
+        }
+    });
+
+}
+
 function css(response, postData) {
 	console.log("Request handler 'css' was called.");
     fs.readFile('./noipasaran.css', function(error, content) {
@@ -78,7 +111,23 @@ function css(response, postData) {
             response.end(content, 'utf-8');
         }
     });
+}
 
+function png(response, postData) {
+    console.log("Request handler 'png' was called.");
+    fs.stat('./noipasaran.png', function(error, stat) {
+        var rs;
+        response.writeHead(200, {
+            'Content-Type' : 'image/png',
+            'Content-Length' : stat.size
+        });
+        rs = fs.createReadStream('./noipasaran.png');
+        util.pump(rs, response, function(err) {
+            if(err) {
+                throw err;
+            }
+        });
+    });
 }
 
 // The function to give the favicon to the browser, not functionnal for now
@@ -102,9 +151,11 @@ function template(response, postData) {
 }
 	
 
-exports.start = start;
+exports.index = index;
 exports.check = check;
 exports.checkhtm = checkhtm;
+exports.about = about;
 exports.css = css;
+exports.png = png;
 exports.favicon = favicon;
 exports.template = template;
